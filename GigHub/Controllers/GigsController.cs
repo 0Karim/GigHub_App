b998e1bf3c93006ae0858470e6b2d0170ce7e1fc
Karim <karimalaa298@gmail.com>
2019-11-09 16:55:28 +0200

    Add Gig Controllere and add it's form as Html Design with bootstrap

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,28 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            GigViewModel model = new GigViewModel
+            {
+                Genres = _context.Genre.ToList()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ GigHub/Models/GigViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class GigViewModel
+    {
+        public string Venue { get; set; }
+        public string Time { get; set; }
+        public string Date { set; get; }
+        public int Genre { set; get; }
+        public IEnumerable<Genre> Genres { set; get; }
+    }
+}
